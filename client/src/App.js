02fb284ee26dd2ed8491b45aadadb91e8dc7da6b
JoJo Zheng <jojozheng1216@hotmail.com>
2018-05-09 09:51:24 -0600

    heroku

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
